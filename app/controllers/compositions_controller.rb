766798c2ef991a54d54d4a8552b6d9e435b8b05d
Yanis Romanovich <thrasherdota2@gmail.com>
2020-06-25 23:56:32 +0300

    Fixed models

--- app/controllers/compositions_controller.rb
+++ app/controllers/compositions_controller.rb
@@ -1,5 +1,30 @@
 class CompositionsController < ApplicationController
   def index
-    @compositions=Composition.all
+    @compositions = Composition.all
+  end
+
+  def show
+    @composition = Composition.find(params[:id])
+  end
+
+  def new
+    @composition = Composition.new
+  end
+
+  def create
+    @composition = Composition.new(post_params)
+    if @composition.save
+      redirect_to @composition
+    else
+      render :new
+    end
+
+  end
+
+  def edit
+  private
+
+  def post_params
+    params.require(:composition).permit(:title, :photo, :description)
   end
 end
