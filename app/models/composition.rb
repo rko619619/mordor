4cd7bbf082d71621c93256115f0996d7df8a0222
Yanis Romanovich <thrasherdota2@gmail.com>
2020-06-30 00:20:11 +0300

    Fixed Compositions

--- app/models/composition.rb
+++ app/models/composition.rb
@@ -1,5 +1,4 @@
 class Composition < ApplicationRecord
-
   has_and_belongs_to_many :categories
   has_many :sections
   has_many :tags
--- db/migrate/20200629211348_create_compositions.rb
+++ db/migrate/20200629211348_create_compositions.rb
@@ -2,8 +2,9 @@ class CreateCompositions < ActiveRecord::Migration[6.0]
   def change
     create_table :compositions do |t|
       t.string :title
-      t.text :description
       t.text :photo
+      t.text :description
+
       t.timestamps
     end
   end
