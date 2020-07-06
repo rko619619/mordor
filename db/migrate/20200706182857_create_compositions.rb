c39712177e38b2e529741aa05a0f5ede1170de47
Yanis Romanovich <thrasherdota2@gmail.com>
2020-07-06 21:30:25 +0300

    Clean version

--- db/migrate/20200706182857_create_compositions.rb
+++ db/migrate/20200706182857_create_compositions.rb
@@ -1,6 +1,10 @@
 class CreateCompositions < ActiveRecord::Migration[6.0]
   def change
     create_table :compositions do |t|
+      t.string :title
+      t.text :body
+      t.text :description
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_182246) do
+ActiveRecord::Schema.define(version: 2020_07_06_182909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 2020_07_06_182246) do
   end
 
   create_table "compositions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.json "image"
   end
 
