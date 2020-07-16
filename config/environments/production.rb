f4090fe691383c3ca8f79f779357c1241c63d57c
Yanis Romanovich <thrasherdota2@gmail.com>
2020-07-17 02:21:00 +0300

    added a config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "mordor_production"
 
-  config.action_mailer.perform_caching = false
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings =
+      {
 
+          :address            => 'smtp.gmail.com',
+          :port               => 587,
+          :domain             => 'gmail.com', #you can also use google.com
+          :authentication     => :plain,
+          :user_name          => 'thrasherdota2@gmail.com',
+          :password           => 'dendixvost'
+      }
+  config.action_mailer.default_url_options = { host: host }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
