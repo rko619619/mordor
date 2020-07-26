d1b5171a6b599d70f1c432ee9d4d8b7acdbfb1ae
Yanis Romanovich <thrasherdota2@gmail.com>
2020-07-26 21:07:19 +0300

    Clean version of search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,6 @@
 class SearchesController < ApplicationController
 
-  def show
-    @compositions = Composition.search(params[:query])
-  end
-
   def index
     @compositions = Composition.search(params[:query])
   end
-
 end
